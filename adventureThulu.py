497cb8257b3e76fe7eed853cf5fc8488f85f5877
zeg-widow <calebroot8@gmail.com>
2023-06-15 15:16:22 -0600

    Changes to the overall structure, as well as added rooms, items, descs. and more.

--- adventureThulu.py
+++ adventureThulu.py
@@ -1,11 +1,33 @@
-def move():
-    pass
+#Caleb Root for IT-140
 
-def room():
-    pass
+#Dictionary nesting to create the rooms and items for the game
+rooms = {
+    'Main Hall':{'item':'Lantern',
+                 'West':'West Ward Hall',
+                 'desc':'You find yourself in a strange dark corridor that seems to lead to many rooms. One, to the east, seems to have a strange sound coming from it. To the west, you see an ajar door leadng to another hall.\nWhat would you like to do?'
+                 },
+    'West ward Hall':{'item' : 'Candle',
+                    'Main Hall': 'East',
+                    'desc':'Upon entering the hall you see '
+                    },
+    'Migos Room':{'item':'Mi-Go Wing',
+                  'North':'West Ward Hall'
+                  },
 
-def welcomer():
-    charName = str(input('Please input your character name:\n'))
-    accept = input('are you sure? y/n\n')
-        if accept = 'y','Y':
-        
+}
+#Global var declarations
+playerRoom = rooms['']
+directions = ['NORTH','SOUTH','EAST','WEST']
+options = ['QUIT','GET','LOOK','Y', 'N']
+playerItems = {}
+look = print('You see a {}\n'.format(playerRoom.get('item')))
+errorMSG = print('Sorry, I didn''t unterstand what you meant. Have you tried GET, LOOK, Y, N, QUIT, or a direction?')
+death = False
+prDesc = print(playerRoom.get('desc'))
+usrName = input('Please enter your player character''s name\n')
+#Main game loop happens while player is alive
+while death != True:
+    print('Welcome,{}, to my first game, HORROR HOTEL, a H.P. Lovecraft inspired text adventure game that runs in console. I hope that you will enjoy it.\n'.format(usrName))
+    playerRoom = rooms['Main Hall']
+    prDesc
+    
