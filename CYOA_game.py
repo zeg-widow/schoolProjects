d344371db1f96b78cf58834598a509b5fa0cadcf
zeg-widow <62684366+zeg-widow@users.noreply.github.com>
2023-06-15 13:37:32 -0400

    Add files via upload

---
+++ CYOA_game.py
@@ -0,0 +1,12 @@
+import tkinter *
+from tkinter import ttk
+
+
+def startup():
+    charName = str(input('Please type in your characters name: \n'))
+
+
+def move():
+
+def window():
+    
---
+++ adventureThulu.py
@@ -0,0 +1,11 @@
+def move():
+    pass
+
+def room():
+    pass
+
+def welcomer():
+    charName = str(input('Please input your character name:\n'))
+    accept = input('are you sure? y/n\n')
+        if accept = 'y','Y':
+        
